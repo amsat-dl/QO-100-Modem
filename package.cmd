0b3c937b6fd4e270ea107cd3366c280c5959a1b4
Tom <tom@m0lte.uk>
2023-07-22 13:47:17 +0100

    Update package.cmd

--- package.cmd
+++ package.cmd
@@ -1,8 +1,10 @@
+del m0lte-amsat-dl-hsmodem.zip
 rd /s /q package
 mkdir package
 copy oscardata\oscardata\bin\Debug\* package
 copy hsmodem\SharedLibs\windows\* package
 mkdir package\audio
 copy hsmodem\audio\* package\audio
-popd
+pushd package
+"C:\Program Files\7-Zip\7z" a ..\m0lte-amsat-dl-hsmodem.zip *
 popd
