490cfe472d62a81c90288cec7407d2a5b9243b06
Tom <tom@m0lte.uk>
2023-07-22 14:10:25 +0100

    Change folder name

--- package.cmd
+++ package.cmd
@@ -1,10 +1,11 @@
 del m0lte-amsat-dl-hsmodem.zip
-rd /s /q package
-mkdir package
-copy oscardata\oscardata\bin\Debug\* package
-copy hsmodem\SharedLibs\windows\* package
-mkdir package\audio
-copy hsmodem\audio\* package\audio
-pushd package
+rd /s /q tmp
+mkdir tmp
+copy oscardata\oscardata\bin\Debug\* tmp
+copy hsmodem\SharedLibs\windows\* tmp
+mkdir tmp\audio
+copy hsmodem\audio\* tmp\audio
+pushd tmp
 "C:\Program Files\7-Zip\7z" a ..\m0lte-amsat-dl-hsmodem.zip *
 popd
+rd /s /q tmp
